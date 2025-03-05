8e0b9b03f5e6105fca9d96cd2d0a5dcfcc9e8f76
wrnsnng <83326476+wrnsnng@users.noreply.github.com>
2025-03-05 23:33:36 +1100

    initial commit

---
+++ supply-docs/.astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/posts/first-post.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post.mdoc&astroContentModuleFlag=true")]]);
+		
